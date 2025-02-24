0f9484300efedffed90fa93e7062c2fe539fd8a8
MartinKurtz <108140017+MartinKurtz@users.noreply.github.com>
2025-02-24 23:38:43 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -9,7 +9,8 @@ RCLONE_REMOTE="ftp.dbit.com"
 # Define source and destination paths
 SOURCE_DIR="$SCRIPT_DIR/source/"
 DEST_DIR="$SCRIPT_DIR/_mirror/$RCLONE_REMOTE/"
-BACK_DIR="$SCRIPT_DIR/_mirror/deleted_$(date +%Y-%m-%d_%H-%M)/"
+BACK_DIR="$SCRIPT_DIR/_mirror/deleted_files/deleted_$(date +%Y-%m-%d_%H-%M)/"
+LOG_FILE="$SCRIPT_DIR/_mirror/rclone_logfiles/$(date +%Y-%m-%d_%H-%M).log"
 
 # Create Backup Dir
 echo "Creating Backup Dir $BACK_DIR"
@@ -17,6 +18,6 @@ mkdir -p "$BACK_DIR"
 
 # Sync files from the source to the destination
 echo "Starting rclone sync from $RCLONE_REMOTE to $DEST_DIR..."
-rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --progress --tpslimit 10
+rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --log-file=$LOG_FILE --log-level=INFO --progress --tpslimit 10
 
 echo "Rclone sync operation is complete."
