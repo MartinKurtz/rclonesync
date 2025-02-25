12274e3f0cfc96d77c8270b0baa7e3441fcf7d5d
MartinKurtz <108140017+MartinKurtz@users.noreply.github.com>
2025-02-25 21:21:05 +0100

    Update sync.sh to create its dirs automagically

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+TPSLIM=10
 # Get the directory of the script
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
@@ -7,17 +7,25 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 RCLONE_REMOTE="ftp.dbit.com"
 
 # Define source and destination paths
-SOURCE_DIR="$SCRIPT_DIR/source/"
-DEST_DIR="$SCRIPT_DIR/_mirror/$RCLONE_REMOTE/"
-BACK_DIR="$SCRIPT_DIR/_mirror/deleted_files/deleted_$(date +%Y-%m-%d_%H-%M)/"
-LOG_FILE="$SCRIPT_DIR/_mirror/rclone_logfiles/$(date +%Y-%m-%d_%H-%M).log"
+MIRROR_DIR="$SCRIPT_DIR/_mirror"
+DEST_DIR="$MIRROR_DIR/$RCLONE_REMOTE/"
+BACK_DIR="$MIRROR_DIR/deleted_files/deleted_$(date +%Y-%m-%d_%H-%M)/"
+LOG_DIR="$MIRROR_DIR/rclone_logfiles/"
+
+
+
+
+
 
 # Create Backup Dir
 echo "Creating Backup Dir $BACK_DIR"
+mkdir -p "$MIRROR_DIR"
+mkdir -p "$DEST_DIR"
 mkdir -p "$BACK_DIR"
+mkdir -p "$LOG_DIR"
 
 # Sync files from the source to the destination
 echo "Starting rclone sync from $RCLONE_REMOTE to $DEST_DIR..."
-rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --log-file=$LOG_FILE --log-level=INFO --progress --fast-list --tpslimit 10
+rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --log-file=$LOG_DIR/$(date +%Y-%m-%d_%H-%M).log --log-level=INFO --progress --fast-list --tpslimit $TPSLIM
 
 echo "Rclone sync operation is complete."
