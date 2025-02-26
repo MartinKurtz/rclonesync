d75fb1211f9a9b38c7bdd1e7c2e1ee63a1462bcc
MartinKurtz <108140017+MartinKurtz@users.noreply.github.com>
2025-02-26 15:58:23 +0100

    Update sync.sh for some beautification

--- sync.sh
+++ sync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 TPSLIM=5
 BWLIM=1000
-USE_RSYNC=0
+USE_RSYNC=1
 # Get the directory of the script
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 # Use the first argument as the rclone remote, or fall back to the default
-RCLONE_REMOTE=""
+RCLONE_REMOTE="ftp.dbit.com"
 
 # Define source and destination paths
 MIRROR_DIR="$SCRIPT_DIR/_mirror"
@@ -28,39 +28,41 @@ mkdir -p "$LOG_DIR"
 
 
 if [ "$USE_RSYNC" -eq 0 ]; then
-  RCLONE VARIANT
-   Sync files from the source to the destination
-  echo "Starting rclone sync from $RCLONE_REMOTE to $DEST_DIR..."
-  rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --log-file=$LOG_DIR/$(date +%Y-%m-%d_%H-%M).log --log-level=INFO --progress --tpslimit $TPSLIM --bwlimit $BWLIM --transfers 1 --checkers 1 --timeout 30s --retries 3
-  echo "Rclone sync operation is complete."
+    #RCLONE VARIANT
+    #Sync files from the source to the destination
+    echo "Starting rclone sync from $RCLONE_REMOTE to $DEST_DIR..."
+    rclone sync "$RCLONE_REMOTE:/" "$DEST_DIR" --backup-dir="$BACK_DIR" --log-file=$LOG_DIR/$(date +%Y-%m-%d_%H-%M).log --log-level=INFO --progress --tpslimit $TPSLIM --bwlimit $BWLIM --transfers 1 --checkers 1 --timeout 30s --retries 3
+    echo "Rclone sync operation is complete."
 
 else
 
-  #RSYNC ALTERNATIVE
-  MOUNT_POINT="$SCRIPT_DIR/source"
-  mkdir -p "$MOUNT_POINT"
-  # Mount the remote storage
-  rclone mount "$RCLONE_REMOTE:" "$MOUNT_POINT" --vfs-cache-mode writes --allow-other --log-file="$LOG_DIR/$(date +%Y-%m-%d_%H-%M)_mount.log" --log-level INFO --bwlimit $BWLIM &
-  MOUNT_PID=$!
-  
-  # Wait for the mount to complete (you can adjust the sleep duration)
-  sleep 5
-  
-  # Check if the mount was successful
-  if ! mount | grep "$MOUNT_POINT" > /dev/null; then
+    #RSYNC ALTERNATIVE
+    echo "using rsync to sync"
+
+    MOUNT_POINT="$SCRIPT_DIR/source"
+    mkdir -p "$MOUNT_POINT"
+    # Mount the remote storage
+    rclone mount "$RCLONE_REMOTE:" "$MOUNT_POINT" --vfs-cache-mode writes --allow-other --log-file="$LOG_DIR/$(date +%Y-%m-%d_%H-%M)_mount.log" --log-level INFO --bwlimit $BWLIM &
+    MOUNT_PID=$!
+
+    # Wait for the mount to complete (you can adjust the sleep duration)
+    sleep 5
+
+    # Check if the mount was successful
+    if ! mount | grep "$MOUNT_POINT" > /dev/null; then
     echo "Failed to mount remote storage. Exiting."
     kill $MOUNT_PID
     exit 1
-  fi
-  
-  # Run rsync to sync files from the mounted remote to the destination
-  rsync -av --progress --timeout=30 --bwlimit $BWLIM --delete --backup --backup-dir="$BACK_DIR" --log-file="$LOG_DIR/$(date +%Y-%m-%d_%H-%M).log" "$MOUNT_POINT/" "$DEST_DIR/"
-  
-  # Unmount the remote storage
-  fusermount -u "$MOUNT_POINT"
-  if [ $? -eq 0 ]; then
+    fi
+
+    # Run rsync to sync files from the mounted remote to the destination
+    rsync -av --progress --timeout=30 --bwlimit $BWLIM --delete --backup --backup-dir="$BACK_DIR" --log-file="$LOG_DIR/$(date +%Y-%m-%d_%H-%M).log" "$MOUNT_POINT/" "$DEST_DIR/"
+
+    # Unmount the remote storage
+    fusermount -u "$MOUNT_POINT"
+    if [ $? -eq 0 ]; then
     echo "Successfully unmounted the remote storage."
-  else
+    else
     echo "Failed to unmount the remote storage."
-  fi
+    fi
 fi
